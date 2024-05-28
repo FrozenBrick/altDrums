5b0138b185b5533b28359867f1633805e5b05716
Bilawal M <bilawal.m@gmail.com>
2024-05-28 14:32:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+var buttonLength = document.querySelectorAll("button")
+// var audio = new Audio ('/sounds/snare.mp3')
+
+for (let index = 0; index < buttonLength.length ; index++) {
+    document.querySelectorAll('button')[index].addEventListener('click', function () {
+        var buttonInner = this.innerText
+        document.querySelector("."+buttonInner).classList.add('pressed')
+        setTimeout(function(){
+            document.querySelector("."+buttonInner).classList.remove('pressed')
+
+        }, 300)
+        
+        var audio = new Audio ('/sounds/'+buttonInner+'.mp3')
+        console.log(audio)
+        audio.play()
+
+
+     
+
+})}
+
+document.addEventListener('keydown', function (event) {
+var keyInner = (event.key)  
+      var audio = new Audio ('/sounds/'+keyInner+'.mp3')
+      document.querySelector("."+keyInner).classList.add('pressed')
+        setTimeout(function(){
+            document.querySelector("."+keyInner).classList.remove('pressed')
+
+        }, 300)
+        console.log(audio)
+        audio.play()
+
+     
+
+})
